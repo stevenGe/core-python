b0b2339f3eae2f471e8c45f53361d238a8103b2a
stevenGe <steven.infortec@gmail.com>
2015-04-03 10:53:59 +0800

    add chapter-1

---
+++ chapter-1/practice_6.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+NAME = 'Steven'
+AGE = '27'
+Favorate_Color = 'Blue'
+
+print "My name is: " + NAME
+print "My age is: " + AGE
+print "My favorate color is: " + Favorate_Color
